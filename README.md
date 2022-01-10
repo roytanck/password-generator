c229b061a6cbe88de834e88aebba5c7d32c84dc9
Roy Tanck <roy.tanck@gmail.com>
2022-01-10 14:31:58 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# password-generator
+A highly opinionated password generator, optimized for speed of use.
+
+Always generates a strong 20-character password with lowercase and uppercase letters, numbers and symbols. Auto-selects the field's contents for quick copying to the clipboard.
+
+## Usage
+Download or clone the code, and open index.html in a modern browser.
---
+++ password.js
@@ -0,0 +1,31 @@
+const init = () => {
+    let field = document.getElementById( 'password' );
+    // character ranges
+    const chars = [
+        'abcdefghijklmnopqrstuvwxyz',
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        '1234567890',
+        '!@#$%^&*?-=_~'
+    ];
+    // initialize the password character array
+    let passwordChars = [];
+    // add one char from each range to satisfy password requirements
+    for( let i=0; i<chars.length; i++ ){
+        passwordChars.push( chars[i][ Math.floor( Math.random() * chars[i].length ) ] );
+    }
+    // concatentate the chars into one string to pick the rest of the characters from
+    let allChars = chars.join('');
+    // add 16 more characters to get a nice, long password
+    for( let i=0; i<16; i++ ){
+        passwordChars.push( allChars[ Math.floor( Math.random() * allChars.length ) ] );
+    }
+    // randomize the array so the first four are not always from range 1-4
+    passwordChars.sort( ( a, b ) => {
+        return ( Math.random() < 0.5 ) ? -1 : 1;
+    } );
+    // create the password string, fill the field and select is for copying
+    field.value = passwordChars.join('');
+    field.select();
+}
+
+document.addEventListener( 'DOMContentLoaded', init, false );
